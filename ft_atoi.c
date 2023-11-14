61b36843244263c7863c3c08b781be912de3773e
Annika Makela <amakela@c1r6p13.hive.fi>
2023-11-14 17:42:35 +0200

    fixed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,45 +6,53 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:39:48 by amakela           #+#    #+#             */
-/*   Updated: 2023/10/31 15:29:30 by amakela          ###   ########.fr       */
+/*   Updated: 2023/11/14 16:22:08 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int  overflow(int neg);
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	nbr;
-	int	neg;
+	int		i;
+	long	nbr;
+	int		neg;
+	long	prev;
 
 	i = 0;
 	nbr = 0;
-	while ((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n')
-		|| (str[i] == '\v') || (str[i] == '\f') || (str[i] == '\r'))
+	neg = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i ++;
 	if ((str[i] == '-') || (str[i] == '+'))
 	{
 		if (str[i] == '-')
-			neg = 1;
+			neg = -1;
 		i ++;
 	}
-	if (!(str[i] >= 48 && str[i] <= 57))
-		return (0);
 	while (str[i] >= 48 && str[i] <= 57)
 	{
-		nbr = 10 * nbr + str[i] - '0';
-		i ++;
+		prev = nbr;
+		nbr = 10 * nbr + str[i++] - '0';
+		if (prev > nbr)
+			return (overflow(neg));
 	}
-	if (neg == 1)
-		nbr = nbr * -1;
-	return (nbr);
+	return ((int)nbr * neg);
+}
+
+static int	overflow(int neg)
+{
+	if (neg == -1)
+		return (0);
+	return (-1);
 }
 /*
 int	main(void)
 {
-	const char	str[] = "+1234";
+	const char	str[] = "0";
 
 	printf("%d\n", atoi(str));
 	printf("%d", ft_atoi(str));
