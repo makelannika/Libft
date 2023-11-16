c78a76c0374b4cd8852cf29bd8d762c2e8642a46
Annika Makela <amakela@c1r4p1.hive.fi>
2023-11-16 16:09:38 +0200

    fixed

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,25 +6,23 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 22:38:14 by amakela           #+#    #+#             */
-/*   Updated: 2023/11/15 19:41:52 by amakela          ###   ########.fr       */
+/*   Updated: 2023/11/16 12:26:19 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*current;
 
-	current = *lst;
-	if (current == NULL)
+	if (!*lst)
 	{
 		*lst = new;
-		new->next = NULL;
 		return ;
 	}
+	current = *lst;
 	while (current->next != NULL)
 		current = current->next;
 	current->next = new;
-	new->next = NULL;
 }
